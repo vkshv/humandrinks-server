9499df480d29e7058f50cbcc3af779e0fd3e14c4
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-05-13 05:40:26 +0300

    multer cfg

--- routes/eventAdminRoutes.js
+++ routes/eventAdminRoutes.js
@@ -4,7 +4,10 @@ const eventAdminController = require('../controllers/eventAdminController')
 const authAdminMiddleware = require('../middleware/authAdminMiddleware')
 
 const router = express.Router()
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 20 * 1024 * 1024 }
+})
 
 function multerErrorHandler(err, req, res, next) {
   console.error('Multer Error Caught:', err)
