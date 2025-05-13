0d75c089da83fbe4568eb9b0e8d3c08cd99755a5
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-05-13 05:03:53 +0300

    log

--- controllers/eventAdminController.js
+++ controllers/eventAdminController.js
@@ -13,35 +13,21 @@ exports.getEventItems = async (req, res) => {
 }
 
 exports.createEventItem = async (req, res) => {
+  console.log('createEventItem')
   try {
     if (req.files?.length) {
       const formData = new FormData()
-      // ---
-      console.log('files.length')
-      // ---
       formData.append('ref', 'events')
       formData.append('field', 'picture')
       formData.append('files', fs.createReadStream(req.files[0].path), req.files[0].originalname)
-      // ---
-      console.log('formData', formData)
-      // ---
       const responseUpload = await http.post('/upload', formData)
-      // ---
-      console.log('responseUpload', responseUpload.data)
-      // ---
       fs.unlinkSync(req.files[0].path)
 
       const data = { data: {
         ...JSON.parse(req.body.data),
         picture: responseUpload.data[0].id
       } }
-      // ---
-      console.log('data', data)
-      // ---
       const response = await http.post('/events', data)
-      // ---
-      console.log('data', data)
-      // ---
       return res.json(response.data)
     } else {
       const data = { data: JSON.parse(req.body.data) }
