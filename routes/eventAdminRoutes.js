91ab5affeb53a975f25d8a29769d7e8585d41d2c
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-05-13 05:30:20 +0300

    log

--- routes/eventAdminRoutes.js
+++ routes/eventAdminRoutes.js
@@ -6,8 +6,27 @@ const authAdminMiddleware = require('../middleware/authAdminMiddleware')
 const router = express.Router()
 const upload = multer({ dest: 'uploads/' })
 
+function multerErrorHandler(err, req, res, next) {
+  console.error('Multer Error:', err)
+  res.status(500).json({ message: 'Multer processing failed', error: err.message })
+}
+
 router.get('/', authAdminMiddleware, eventAdminController.getEventItems)
-router.post('/', authAdminMiddleware, upload.any(), eventAdminController.createEventItem)
+router.post(
+  '/',
+  (req, res, next) => {
+    console.log('ROUTE POST / triggered')
+    next()
+  },
+  authAdminMiddleware,
+  upload.any(),
+  multerErrorHandler,
+  (req, res, next) => {
+    console.log('Multer passed:', req.files)
+    next()
+  },
+  eventAdminController.createEventItem
+)
 router.put('/:id', authAdminMiddleware, upload.any(), eventAdminController.updateEventItem)
 router.delete('/:id', authAdminMiddleware, eventAdminController.deleteEventItem)
 router.get('/categories', authAdminMiddleware, eventAdminController.getEventCategories)
