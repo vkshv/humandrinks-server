f14524a2bed85841e72b09959acb8d4cc8b7f4a3
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-05-23 01:42:17 +0300

    rm log

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -13,12 +13,10 @@ exports.activateReferralProgram = async (req, res) => {
   let referrerUser = null
   let referralProgram = null
   try {
-    console.log('referralTelegramId', referralTelegramId)
     const response_referral = await http.get(`/visitors?filters[telegramId]=${referralTelegramId}`)
     if (!response_referral.data.data.length) return res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Пользователь не найден' })
 
     referralUser = response_referral.data.data[0]
-    console.log('referralUser', referralUser.telegramId)
     if (!referralUser.referralProgram) return res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Пользователь не является рефералом' })
     if (referralUser.referralProgram.activated === true) return res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Реферальная программа уже активирована' })
 
@@ -26,12 +24,10 @@ exports.activateReferralProgram = async (req, res) => {
     if (!response_referrer.data.data.length) return res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Реферер пользователя не найден' })
 
     referrerUser = response_referrer.data.data[0]
-    console.log('referrerUser', referrerUser.telegramId)
     const response_program = await http.get(`/referral-programs?filters[slug]=${referralUser.referralProgram.programSlug}`)
     if (!response_program.data.data.length) return res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Реферальная программа не найдена' })
 
     referralProgram = response_program.data.data[0]
-    console.log('referralProgram', referralProgram.slug)
   } catch (error) {
     return res.status(STATUS_CODE.INTERNAL_SERVER_ERROR).json({ message: STATUS_TEXT[STATUS_CODE.INTERNAL_SERVER_ERROR] })
   }
@@ -41,7 +37,6 @@ exports.activateReferralProgram = async (req, res) => {
   // реализовать логику действий согласно настройкам
 
   try {
-    console.log('1')
     const referral_data = {
       data: {
         referralProgram: {
@@ -50,18 +45,14 @@ exports.activateReferralProgram = async (req, res) => {
         }
       }
     }
-    console.log('2')
     await http.put(`/visitors/${referralUser.documentId}`, referral_data)
-    console.log('3')
 
     const referrer_data = {
       data: {
         bonus: (referrerUser.bonus ?? 0) + referralProgram.referrer_bonus_value
       }
     }
-    console.log('4')
     await http.put(`/visitors/${referrerUser.documentId}`, referrer_data)
-    console.log('5')
 
     return res.json({})
   } catch (error) {
